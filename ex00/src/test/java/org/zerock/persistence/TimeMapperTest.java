076fec863290d6748d4ecac540cdff6f3c89dc26
rmsdn990422 <rmsdn990422@gmail.com>
2022-12-11 01:00:53 +0900

    spring 3일차

---
+++ ex00/src/test/java/org/zerock/persistence/TimeMapperTest.java
@@ -0,0 +1,31 @@
+package org.zerock.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.mapper.TimeMapper;
+
+import lombok.Setter;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+
+public class TimeMapperTest {
+	@Setter
+	@Autowired
+	private TimeMapper timeMapper;
+	
+	@Test
+	public void testGetTime() {
+		System.out.println(timeMapper.getClass().getName());
+		System.out.println(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		System.out.println("getTime2");
+		System.out.println(timeMapper.getTime2());
+	}
+}
---
+++ ex03/src/test/java/org/zerock/mapper/BoardMapperTest.java
@@ -0,0 +1,47 @@
+package org.zerock.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class BoardMapperTest {
+	@Setter
+	@Autowired
+	private BoardMapper mapper;
+	
+	@Test
+	public void testGetList() {
+		mapper.getList().forEach(board -> System.out.println(board));
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		mapper.insert(board);
+		
+		System.out.println(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 select key");
+		board.setContent("새로 작성하는 내용 select key");
+		board.setWriter("newbie");
+		
+		mapper.insertSelectKey(board);
+		
+		System.out.println(board);
+	}
+}
